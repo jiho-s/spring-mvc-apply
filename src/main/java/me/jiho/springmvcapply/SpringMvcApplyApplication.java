644a4f11b90c9f80b31580196ec35c1205379385
jiiiho <tlswlgh120@naver.com>
2020-05-19 11:50:29 +0900

    Initial commit

---
+++ src/main/java/me/jiho/springmvcapply/SpringMvcApplyApplication.java
@@ -0,0 +1,13 @@
+package me.jiho.springmvcapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcApplyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jiho/springmvcapply/SpringMvcApplyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jiho.springmvcapply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
