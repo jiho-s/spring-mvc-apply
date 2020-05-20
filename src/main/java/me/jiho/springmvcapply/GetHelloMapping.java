2450b70e63d766efec18714b40a0c2ded797e7a2
jiiiho <tlswlgh120@naver.com>
2020-05-20 17:28:07 +0900

    HTTP Request Mapping

---
+++ src/main/java/me/jiho/springmvcapply/GetHelloMapping.java
@@ -0,0 +1,16 @@
+package me.jiho.springmvcapply;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@RequestMapping(method = RequestMethod.GET, value = "/hello")
+public @interface GetHelloMapping {
+
+}
--- src/test/java/me/jiho/springmvcapply/SampleControllerTest.java
+++ src/test/java/me/jiho/springmvcapply/SampleControllerTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultHandler;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,9 +26,8 @@ class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception{
-        mockMvc.perform(get("/hi/hello"))
+        mockMvc.perform(get("/hello"))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello"));
+                .andExpect(status().isOk());
     }
 }
